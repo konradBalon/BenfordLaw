c8aec3e99d273c5e5653f60ac0d19c1b8aeba221
konrad <konrad.balon@gmail.com>
2021-12-17 17:43:59 +0100

    added Chart.JS, pandas dataframes

--- benny/views.py
+++ benny/views.py
@@ -1,5 +1,6 @@
 from http.client import HTTPResponse
 
+import pandas as pd
 from django.core.exceptions import ValidationError
 from django.db.models import FilePathField
 from django.shortcuts import render, redirect
@@ -52,8 +53,43 @@ class PickerView(View):
 class HandleFileView(View):
 
     def get(self, request, document):
-        #         load file
         document = Document.objects.filter(document=f'documents/{document}').first()
-        print(document.document)
-        return render(request, 'benny/handle-file.html', {'document': document})
+        handler = HandleFile(document=document)
+        handler.load_data_frame()
+        column = handler.df.columns[2]
+        print(column)
+        file_data = handler.get_benford_data(column)
+        benford_data = [0.301, 0.176, 0.125, 0.097, 0.079, 0.067, 0.058, 0.051, 0.046]
+        labels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        return render(request, 'benny/handle-file.html', {'labels': labels,
+                                                          'benford_data': benford_data, 'file_data': file_data})
 
+
+class HandleFile:
+
+    def __init__(self, document):
+        self.document = document
+        self.df = None
+
+    def load_data_frame(self):
+        df = pd.read_csv(self.document.document.path, on_bad_lines='skip', sep=None, engine='python')
+        self.df = df
+
+    def get_headers(self):
+        headers = self.df.columns
+        return headers
+
+    def get_benford_data(self, column):
+        benford_data = []
+
+        # convert to str and keep first character
+        df = self.df[column].astype(str).str[:1]
+
+        dframe = pd.DataFrame(data=df)
+
+        for _ in range(1, 10):
+            # count values for each number then divide by all rows count
+            val = (dframe[dframe[column] == str(_)].shape[0]) / (df.shape[0])
+            benford_data.append(val)
+
+        return benford_data
