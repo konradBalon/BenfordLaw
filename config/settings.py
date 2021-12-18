f9b04cba194cf34f80910068b2078e1b3d87e8d1
konrad <konrad.balon@gmail.com>
2021-12-18 14:41:05 +0100

    dockerfile updated

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!2$i6d5ke3i$*2un9%-tjihx*vyo&8xacnm4g!y$c70b1)hhk8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
